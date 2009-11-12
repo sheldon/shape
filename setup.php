ccc9ffbde9b9f52f44882fc2faa4c7e420b398b7
charles marshall <charlesmarshall@charles-marshall.local>
2009-11-12 11:43:10 +0000

    bug fix - had an extra ) on the end

--- setup.php
+++ setup.php
@@ -4,7 +4,7 @@
  */
 define('SHAPE_BRANCH', 'v1');
 define("SHAPE_DIR", dirname(__FILE__));
-if(is_readable(SHAPE_DIR.'/.git/refs/heads/'.SHAPE_BRANCH)) $revision = file_get_contents(SHAPE_DIR.'/.git/refs/heads/'.SHAPE_BRANCH));
+if(is_readable(SHAPE_DIR.'/.git/refs/heads/'.SHAPE_BRANCH)) $revision = file_get_contents(SHAPE_DIR.'/.git/refs/heads/'.SHAPE_BRANCH);
 else $revision = "";
 define('SHAPE_REVISION', $revision);
 ?>
