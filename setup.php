4eb31bc0388ab1dd952e1f7ce8d9b19269fec8d3
charles marshall <charlesmarshall@charles-marshall.local>
2009-11-12 11:38:07 +0000

    some details about the plugin - fetch the hash the git repo is currently on

---
+++ setup.php
@@ -0,0 +1,10 @@
+<?
+/**
+ * Load up some information about the plugin, branch, directory etc 
+ */
+define('SHAPE_BRANCH', 'v1');
+define("SHAPE_DIR", dirname(__FILE__));
+if(is_readable(SHAPE_DIR.'/.git/refs/heads/'.SHAPE_BRANCH)) $revision = file_get_contents(SHAPE_DIR.'/.git/refs/heads/'.SHAPE_BRANCH));
+else $revision = "";
+define('SHAPE_REVISION', $revision);
+?>
