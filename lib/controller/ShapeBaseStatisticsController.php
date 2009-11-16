0dd991497a8f2c94f18ad4a438275a756551556e
charles marshall <charlesmarshall@charles-marshall.local>
2009-11-16 10:59:50 +0000

    statistics controllers

---
+++ lib/controller/ShapeBaseStatisticsController.php
@@ -0,0 +1,5 @@
+<?php
+class ShapeBaseStatisticsController extends ShapeController {
+
+}
+?>
---
+++ resources/app/controller/shape/ShapeStatisticsController.php
@@ -0,0 +1,5 @@
+<?php
+/**
+*/
+class ShapeStatisticsController extends ShapeBaseStatisticsController{}
+?>
