1c0f59dee1a378aee18b104efcd7195405c6a31c
charles marshall <charlesmarshall@charles-marshall.local>
2009-11-17 15:01:14 +0000

    find and create a list of all controllers

--- setup.php
+++ setup.php
@@ -1,5 +1,4 @@
 <?
-
 /**
  * Load up some information about the plugin, branch, directory etc 
  */
@@ -9,4 +8,34 @@ define("SHAPE_DIR", dirname(__FILE__));
 if(is_readable(SHAPE_DIR.'/.git/refs/heads/'.SHAPE_BRANCH)) $revision = file_get_contents(SHAPE_DIR.'/.git/refs/heads/'.SHAPE_BRANCH);
 else $revision = "";
 define('SHAPE_REVISION', $revision);
+
+/**
+ * Find and log all the available modules
+ * - check cache
+ * - glob the current directory
+ * - check its readable
+ * - make sure its a controller
+ * - add to array
+ */
+$cache = new WaxCacheFile(CACHE_DIR."shape", "forever", 'cache', CACHE_DIR."shape/modules.cache");
+
+if($found = $cache->get()) $found = unserialize($found);
+else{
+  $found = array();
+  $path = SHAPE_DIR."/resources/app/controller/".PLUGIN_NAME."/*.php";
+  foreach(glob($path) as $file){
+    if(is_readable($file)){
+      $name = substr(basename($file),0,-4);
+      $model = new $name(false);
+      if($model instanceOf ShapeBaseController){
+        $common_name = strtolower(str_ireplace("controller", "", str_ireplace(PLUGIN_NAME, "", $name)));
+        $found[$common_name] = $name; 
+      }
+    }  
+  }
+  $cache->set(serialize($found));
+}
+
+define("CONTROLLER_LIST", serialize($found));
+
 ?>
