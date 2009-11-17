54648babb80af5c5b7632ee361bcfbecfaca78fa
charles marshall <charlesmarshall@charles-marshall.local>
2009-11-17 15:00:59 +0000

    remove menu from the list of permissions, not needed

--- lib/controller/ShapeBaseController.php
+++ lib/controller/ShapeBaseController.php
@@ -8,6 +8,7 @@ class ShapeBaseController extends WaxController {
   public $use_plugin = "shape"; //this should not be needed - see comment below regarding [DEPRECATION]
   
   public $controller_list=array();
+  
   public $user_model = "ShapeUser"; //user model
   public $user_table = "shape_user"; //user table
   public $login_path = "/shape/login"; //path to login
@@ -15,7 +16,7 @@ class ShapeBaseController extends WaxController {
   public $current_user = false; //logged in user object, this set to false means an unauthenticated request and it should be checked on every request
   public $authenticate=false;
   
-  public $base_permissions = array("enabled","menu","create","view","delete","edit"); //base permissions to be merged with extended ones
+  public $base_permissions = array("enabled","create","view","delete","edit"); //base permissions to be merged with extended ones
   public $permissions = array(); //stub for extendable permissions, can be added to extended controllers easily
   
   public $model_class; //class name
