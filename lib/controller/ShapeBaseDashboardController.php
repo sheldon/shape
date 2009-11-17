c0e0e06b12045e9e9fb439b40929a52f48d9ea1e
charles marshall <charlesmarshall@charles-marshall.local>
2009-11-17 19:48:40 +0000

    moved login function to dashboard

--- lib/controller/ShapeBaseDashboardController.php
+++ lib/controller/ShapeBaseDashboardController.php
@@ -1,5 +1,34 @@
 <?php
 class ShapeBaseDashboardController extends ShapeController {
+  
+  /**
+   * login function
+   */
+  public function login(){
+    $user = new ShapeUser;
+    $this->login_form = new WaxForm($user);
+    /**
+     * form is posted so check the values
+     */
+    if($user->is_posted() && ($values = Request::param('shape_user')) ){
+      /**
+       * Only check the database if both values are set - reduce lookups
+       * Inform users what they are missing
+       * If all good then redirect to where they where trying to get to, or the default
+       */
+      if(!$values['username'] && !$values['password']) Session::add_error('Please enter username and password');
+      elseif(!$values['username'] && $values['password']) Session::add_error('Please enter a username');
+      elseif($values['username'] && !$values['password']) Session::add_error('Please enter a password');
+      else{
+        if($this->authenticate->verify($values['username'], $values['password'])){
+          Session::add_message('Welcome Back '.$values['username']);
+          $this->current_user = $this->authenticate->get_user();
+          if($redirect = Session::get('shape_redirect_to')) $this->redirect_to($redirect);
+          else $this->redirect_to($this->login_success);
+        }else Session::add_error('Sorry, those details cannot be found, please try again.');
+      }
+    }
+  }
   /**
    * logout function, clear the sessions etc
    */
