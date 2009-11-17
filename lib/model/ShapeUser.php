125cade6f2449a5c01099ee1295f102272f724c8
charles marshall <charlesmarshall@charles-marshall.local>
2009-11-17 17:36:28 +0000

    let the user have permissions

--- lib/model/ShapeUser.php
+++ lib/model/ShapeUser.php
@@ -8,11 +8,23 @@ class ShapeUser extends WaxModel {
     $this->define("surname", "CharField");
     
     $this->define("pages", "ManyToManyField", array("target_model"=>"ShapePage"));
-    //$this->define("permissions", "HasManyField", array("target_model"=>"ShapePermission"));
+    $this->define("permissions", "HasManyField", array("target_model"=>"ShapePermission"));
   }
   
 	public function fullname() {
 	  return $this->firstname." ".$this->surname;
 	}
+	
+	public function permissions($controller){
+	  $path = CACHE_DIR."/shape/permission-".$this->primval.".cache";
+    $cache = new WaxCacheFile(CACHE_DIR."shape", 3600, 'cache', $path);
+    $modules = array();
+    if(!$found = $cache->valid()){
+      foreach($this->permissions as $perm) $modules[$perm->controller][$perm->action] = true;
+      $cache->set(serialize($modules));
+    }else $modules = unserialize($found);
+    return $modules;
+	}
+	
 	public function title(){return $this->fullname ." (".$this->username.")";}
 }?>
