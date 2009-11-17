866e007d3ad3e93c51bc47940e2417d8a51f9ebd
charles marshall <charlesmarshall@charles-marshall.local>
2009-11-17 20:05:42 +0000

    added an allowed field - so black listing is possible

--- lib/model/ShapePermission.php
+++ lib/model/ShapePermission.php
@@ -3,6 +3,7 @@ class ShapePermission extends WaxModel {
   public function setup() {
     $this->define("controller", "CharField");
     $this->define("action", "CharField");    
+    $this->define("allowed", "BooleanField");    
   }
   
   public function title(){return $this->controller . " ".$this->action;}
