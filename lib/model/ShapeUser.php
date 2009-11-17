2aa55728f2856878297815f7938c63423c02521e
charles marshall <charlesmarshall@charles-marshall.local>
2009-11-17 20:24:46 +0000

    dont cache the db join atm - rename param to $class rather than controller

--- lib/model/ShapeUser.php
+++ lib/model/ShapeUser.php
@@ -15,15 +15,10 @@ class ShapeUser extends WaxModel {
 	  return $this->firstname." ".$this->surname;
 	}
 	
-	public function permissions($controller){
-	  $path = CACHE_DIR."/shape/permission-".$this->primval.".cache";
-    $cache = new WaxCacheFile(CACHE_DIR."shape", 3600, 'cache', $path);
+	public function permissions($class){
     $modules = array();
-    if(!$found = $cache->valid()){
-      foreach($this->permissions as $perm) $modules[$perm->controller][$perm->action] = $perm->allowed;
-      $cache->set(serialize($modules));
-    }else $modules = unserialize($found);
-    return $modules;
+    foreach($this->permissions as $perm) $modules[$perm->classname][$perm->action] = $perm->allowed;
+    return $modules[$class];
 	}
 	
 	public function title(){return $this->fullname ." (".$this->username.")";}
