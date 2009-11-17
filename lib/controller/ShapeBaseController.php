96ea680ce0e02e85b752d27492c07049ead683ae
charles marshall <charlesmarshall@charles-marshall.local>
2009-11-17 11:43:24 +0000

    2 vars to check track of model state - for better notifications on the front end

--- lib/controller/ShapeBaseController.php
+++ lib/controller/ShapeBaseController.php
@@ -25,6 +25,8 @@ class ShapeBaseController extends WaxController {
   public $filters = array(); //columns to filter on
   public $string_field = "title"; //field used in list
   public $multi_level = false; //this will trigger fetching of multi level nav - mainly designed for the pages system
+  public $model_posted=false;
+  public $model_saved=false;
   
   public $site_name; //$_SERVER['http_host']
   public $widgets = array("shape/pages/_search", "shape/statistics/_analytics", "shape/dashboard/_summary", "shape/pages/_recentpages"); //default widget
